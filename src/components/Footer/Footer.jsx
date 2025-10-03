8e9e768ef4d3751ad6a88106ac913cb99935ec19
parkjihun22 <eyeful31@naver.com>
2025-10-03 12:26:22 +0900

    feat : 푸터시공사수정

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -72,7 +72,7 @@ const Footer = () => {
           {/* 시행사/시공사 */}
           <div className={styles.mobileCompany}>
             <div>
-              <strong>시&nbsp;공&nbsp;사</strong> (주)한양건설
+              <strong>시&nbsp;공&nbsp;사</strong> (주)BS한양(예정)
             </div>
           </div>
 
@@ -128,7 +128,7 @@ const Footer = () => {
               </div>
               <div className={styles.companyInfo}>
                 <div>
-                  <strong>시&nbsp;공&nbsp;사</strong> (주)한양건설 
+                  <strong>시&nbsp;공&nbsp;사</strong> (주)BS한양(예정) 
                 </div>
               </div>
               <div className={styles.buttonRow}>
